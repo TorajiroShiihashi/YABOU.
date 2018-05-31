8d7396a2a66559d901b2b5fb402a7db4be566bb6
Torajiro Shiihashie <uptempo1996@gmail.com>
2018-05-31 18:07:22 +0900

    Add hello

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/YABOU.' => "home#top" ;
+ root 'application#hello'
 
 end
