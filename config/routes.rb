507116389079b2260c6a4182e9f1da0144763bf4
Torajiro Shiihashie <uptempo1996@gmail.com>
2018-06-01 00:56:00 +0900

    Refactoring

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   resources :microposts
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/YABOU.' => "home#top" ;
- root 'application#hello'
-
+  get '/YABOU.' => "home#top"
+  get '/YABOU./about' => "home#about"
 end
