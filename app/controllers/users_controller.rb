e3ead8d34284cbbf3c6d7bc2b671ce974317b108
Torajiro Shiihashie <uptempo1996@gmail.com>
2018-06-02 00:16:26 +0900

    Add a full_title helper

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
+  attr_accessor :name, :email
 
+  def initialize(attributes = {})
+    @name  = attributes[:name]
+    @email = attributes[:email]
+  end
   # GET /users
   # GET /users.json
   def index
